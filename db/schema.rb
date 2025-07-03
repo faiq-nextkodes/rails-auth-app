365e230c2d4c7e9d325d1f7f663ae80190446b7c
Faiq <your.faiq@nextkodes.com>
2025-07-03 18:03:11 +0500

    Second Commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_02_155430) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_03_123123) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.integer "price"
@@ -21,7 +21,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_02_155430) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "user_name", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
