4b10f03a29a5186f7e1feaa8450d12bfc2e2881c
Faiq <your.faiq@nextkodes.com>
2025-07-04 14:56:43 +0500

    Final Commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-Product.create([
-  { name: "Laptop", price: 1200 },
-  { name: "Mouse", price: 25 },
-  { name: "Keyboard", price: 45 }
-])
+# Product.create([
+#   { name: "Monitor", price: 70000 },
+#   { name: "RTX 5090", price: 450000 },
+#   { name: "Wallet", price: 4 }
+# ])
+
+# User.create([
+#   {email: "test2@example.com", password: "test123"}
+# ])
